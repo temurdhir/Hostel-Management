0ccf3689749ed8660d1661e1cdd3bfa66cd94cc7
temur <temur@DESKTOP-O3F9GFL>
2019-07-28 16:04:11 +0530

    done

--- src/main/java/main/Hostel.java
+++ src/main/java/main/Hostel.java
@@ -40,17 +40,29 @@ public class Hostel{
 		               String[] splited = line.split("\\s+");
 		               String checkName = splited[0];
 		               //write your code here !!!
+		               
+	                    if(checkName.equals(name) ){
+	                        return true;
 //		               compare check name with name and return true if present and false if not
 	               }
-	               
+	               }
 	               
 	            }catch(Exception e){
 	                System.out.println(e);
 	            }
-			return true;
+			return false;
 	   }
        public static void allotHostel(){
     	   //write your code here!!!
+    	   try
+   		{	
+   			writedata();
+           }
+   		catch(IOException e)
+   		{
+               //print
+               System.out.println(e);    
+           }
     	   
        }
 
@@ -76,6 +88,7 @@ public class Hostel{
     	   boolean chk = true;
     	   
     	   //write your code here
+    	   chk = readData(name);
     	   
     	   return chk;
         }
